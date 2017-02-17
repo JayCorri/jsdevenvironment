58a1327e997ef3cf5694b33ccb66753219cd6335
Chris <c_diaz86@hotmail.com>
2017-02-17 12:35:39 -0800

    adding mockdata and mockdata endpoints to api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 // import numeral from 'numeral';
 
 // const courseValue = numeral(1000).format('$0,0.00');
@@ -13,11 +13,25 @@ getUsers().then(result => {
         usersBody += `<tr>
         <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
         <td>${user.id}</td>
-        <td>${user.fName}</td>
-        <td>${user.lName}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
         <td>${user.email}</td>
         <tr>`
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    //must use array.from to create real array from DOM collection
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event){
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
